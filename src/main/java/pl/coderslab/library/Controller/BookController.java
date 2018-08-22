fe051cec556560969b7ecee0a0ccd53fea84e0ab
LeszekLuksza <leszekluksza90@gmail.com>
2018-08-22 23:36:06 +0200

    Add findAllBook method

--- src/main/java/pl/coderslab/library/Controller/BookController.java
+++ src/main/java/pl/coderslab/library/Controller/BookController.java
@@ -1,6 +1,26 @@
 package pl.coderslab.library.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.library.Model.Book;
+import pl.coderslab.library.Service.BookRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/all")
+    public List<Book> getAllBooks()
+    {
+        return bookRepository.findAll();
+    }
 
 
 }
