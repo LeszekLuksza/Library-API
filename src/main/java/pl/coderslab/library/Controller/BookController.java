1d790e85a1baad6734db9c3b553a2d4589ecae13
LeszekLuksza <leszekluksza90@gmail.com>
2018-08-22 23:51:59 +0200

    Update mapping for getAllBooks. Add getBookByIsbn

--- src/main/java/pl/coderslab/library/Controller/BookController.java
+++ src/main/java/pl/coderslab/library/Controller/BookController.java
@@ -1,26 +1,32 @@
 package pl.coderslab.library.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.library.Model.Book;
 import pl.coderslab.library.Service.BookRepository;
 
+import java.math.BigInteger;
 import java.util.List;
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping(value = "/book")
 public class BookController {
 
-    @Autowired
+    private final
     BookRepository bookRepository;
 
-    @GetMapping("/all")
-    public List<Book> getAllBooks()
-    {
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping(value={"","/","all"})
+    public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
-
+    @GetMapping(value="/{isbn}")
+    public Book getBookByIsbn(@PathVariable BigInteger isbn){
+        return bookRepository.findBookByIsbn(isbn);
+    }
 }
