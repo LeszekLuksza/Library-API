5488b472bc56d325d8d8bbebbde592e4910ca640
LeszekLuksza <leszekluksza90@gmail.com>
2018-08-22 23:36:41 +0200

    Change Long To BigInteger

--- src/main/java/pl/coderslab/library/Service/BookRepository.java
+++ src/main/java/pl/coderslab/library/Service/BookRepository.java
@@ -1,9 +1,10 @@
 package pl.coderslab.library.Service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.library.Model.Book;
 
-import java.awt.print.Book;
+import java.math.BigInteger;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-    public Book findBookByIsbn(Long isbn);
+public interface BookRepository extends JpaRepository<Book, BigInteger> {
+    public Book findBookByIsbn(BigInteger isbn);
 }
