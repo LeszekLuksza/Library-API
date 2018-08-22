a099310bd39ca14574dad1a7fdca72ae7e38e15e
LeszekLuksza <leszekluksza90@gmail.com>
2018-08-22 23:35:34 +0200

    Add getters and setters

--- src/main/java/pl/coderslab/library/Model/Book.java
+++ src/main/java/pl/coderslab/library/Model/Book.java
@@ -42,6 +42,42 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public BigInteger getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(BigInteger isbn) {
+        this.isbn = isbn;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
 }
 
 
