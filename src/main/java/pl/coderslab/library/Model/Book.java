49bff7208d5ef774d617c2398643666d974473a8
LeszekLuksza <leszekluksza90@gmail.com>
2018-08-22 22:46:07 +0200

    Add Book Model

---
+++ src/main/java/pl/coderslab/library/Model/Book.java
@@ -0,0 +1,41 @@
+package pl.coderslab.library.Model;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "library")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name="publisher")
+    private String publisher;
+
+    @Column(name="isbn")
+    @Length(min=13, max=13)
+    private Long isbn;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String publisher, Long isbn) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
+
+}
+
+
